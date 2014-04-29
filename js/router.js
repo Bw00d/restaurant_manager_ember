6145e6f137e3a857f76f79b2e207d59c70901609
Nolan Miller & Brentwood Reid <student@epicodus.com>
2014-04-29 09:54:46 -0700

    Display model data

--- js/router.js
+++ js/router.js
@@ -1,3 +1,8 @@
 Tables.Router.map(function(){
   this.resource('tables', { path: '/'});
-})
+});
+Tables.TablesRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('table');
+  }
+});
