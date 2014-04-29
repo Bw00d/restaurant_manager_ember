e55377689c2c4cf571f824fd84f33e88886cdcd7
Nolan Miller & Brentwood Reid <student@epicodus.com>
2014-04-29 09:48:59 -0700

    Add fixtures

--- js/application.js
+++ js/application.js
@@ -1,1 +1,3 @@
 window.Tables = Ember.Application.create({LOG_TRANSITIONS: true});
+
+Tables.ApplicationAdapter = DS.FixtureAdapter.extend();
--- js/models/table.js
+++ js/models/table.js
@@ -1,3 +1,23 @@
 Tables.Table = DS.Model.extend({
   number: DS.attr('integer')
 });
+
+Tables.Table.FIXTURES = [
+  {
+    id: 1,
+    number: 1
+  },
+  {
+    id: 2,
+    number: 2
+  },
+  {
+    id: 3,
+    number: 3
+  },
+  {
+    id: 4,
+    number: 4
+  }
+
+]
